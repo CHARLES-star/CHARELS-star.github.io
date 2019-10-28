d9a2d7b58ece352eee66d02d3405bb05052fc9cc
charles kaetzel on school computer <charles.kaetzel@bellevuecollege.edu>
2019-10-28 12:20:15 -0700

    changes

---
+++ javascript-tests/primitives.js
@@ -0,0 +1,23 @@
+const myString = "Learning Javascript in class."
+
+const myFirstNumericVariable = 3;
+var oddStyle = "this is the way we used to do it.";
+let newChangeableStyle = "this allows you to declare a variable that can be changed.";
+const constantVariable = "This variable can not be changed. Except...";
+const quotable = 'He said, "hello".';
+const myBoolean = true;
+const myNull = null;
+let myUndefined = "Learning more Javascript in class.";
+
+console.log(myString);
+console.log('TYPE', typeof myString);
+console.log(myFirstNumericVariable);
+console.log('TYPE', typeof myFirstNumericVariable);
+console.log(myBoolean);
+console.log('TYPE', typeof myBoolean);
+console.log(myNull)
+console.log('TYPE', typeof myNull)
+console.log(myUndefined)
+console.log('TYPE', typeof myUndefined)
+console.log("add 3 +25", myFirstNumericVariable + 25)
+console.log("add undefined", myUndefined + 25)
