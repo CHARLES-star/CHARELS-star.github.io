06d064896c4640dfa72a105ccf0e155885e6a350
charles kaetzel on school computer <charles.kaetzel@bellevuecollege.edu>
2019-11-13 12:19:46 -0800

    changes

--- javascript-tests/function-basics.js
+++ javascript-tests/function-basics.js
@@ -1,9 +1,33 @@
 window.onload = () => {
-    const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');
-    const functionCallsAction = document.getElementById('functionCallsAction');
-    function simple() {
-        console.log('simple');
-        simpleFunctionDisplay.textContent = "simple";
-    }
-    functionCallsAction .onclick = simple;   
+    // All code goes in here.
 }
+
+
+function simpleFunction() {
+    console.log("functions should consist of statments designed to preform a single task.");
+}
+
+function functionParameters(value)  {
+    console.log(value);
+}
+
+function functionReturn(addResult) {
+    return  "The add function returned." + addResult;
+}
+
+function add(a, b) {
+    return a + b
+}
+
+simpleFunction();
+
+functionParameters("Many functions take parameters.");
+
+const addResult = add(2, 3);
+
+console.log('Add Result', addResult)
+
+const returnValue = functionReturn(addResult);
+
+console.log(returnValue)
+
